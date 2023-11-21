b64d7d9e5ee488d8013f53c02bfbe07d8b2a3581
Juan Holy <juanholydesouza@gmail.com>
2023-11-20 23:05:06 -0400

    feat: OrderItem entity

---
+++ src/main/java/com/JuanHDSM/Vendas/domain/entities/OrderItem.java
@@ -0,0 +1,24 @@
+package com.JuanHDSM.Vendas.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private Integer quantity;
+}
